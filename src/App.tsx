90ffd9690a07d18369f024f2c2ef6143b04cbba5
dankostecki <iluzjadk@wp.pl>
2025-07-11 12:09:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
 
+const App: React.FC = () => (
+  <div className="App">
+    <Dashboard />
+  </div>
+);
+
+export default App;
