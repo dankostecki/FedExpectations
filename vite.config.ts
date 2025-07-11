75046f8ccb58a65c00ec9805a1917ce55b526ffc
dankostecki <iluzjadk@wp.pl>
2025-07-11 12:32:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Base path for your GitHub Pages repo
+  // Zmień na właściwą nazwę swojego repozytorium GitHub
   base: '/FedExpectations/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
