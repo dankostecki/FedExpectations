c207a5a9512c6e53ed03669b0aa4b04c06490e98
dankostecki <iluzjadk@wp.pl>
2025-07-11 12:00:46 +0200

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,14 @@
+export interface InterestData {
+  period: string;
+  "25th Percentile": number;
+  Median: number;
+  "75th Percentile": number;
+  respondents: number;
+}
 
+export interface RawDataPoint {
+  quarter: string;
+  [metric: string]: number | null | string;
+}
+
+export type MetricColors = Record<string, string>;
